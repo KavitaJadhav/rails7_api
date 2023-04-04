b1f8df27480d09e13be1f8844a0715808dd7225a
KavitaJadhav <jadhavkavita206@gmail.com>
2023-04-04 08:25:24 +0530

    Tried encrypted database model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # encrypts :last_name
+  # encrypts :last_name, deterministic: true,
+  # encrypts :last_name, deterministic: true,  downcase: true
+  # encrypts :last_name, deterministic: true,  ignore_case: true
+  # 
+end
